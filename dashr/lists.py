6505de672204e4c732820a9e633b902c2564ac40
Joshua Shaak-Miller <shaakmiller@hotmail.com>
2017-11-06 07:41:02 -0500

    Adding of replace functionality

--- dashr/lists.py
+++ dashr/lists.py
@@ -1,5 +1,5 @@
 class Lists(object):
-    """Interface to the Dasher.tv Squares API
+    """Interface to the Dasher.tv Lists API
     Args:
         client (Client): The dashr client to be used for requests
     """
@@ -11,6 +11,17 @@ class Lists(object):
         """Returns the details of a given square."""
         return self._client.request('/squares/info', {'square': square_id})
 
+    def add(self, square_id, key, value, index_value):
+        """Adds a value of a given list."""
+        params = {
+            'square': square_id,
+            'list_item': key,
+            'properties': value,
+            'index': index_value
+
+        }
+        return self._client.request('/lists/add', params)
+
     def edit(self, square_id, key, value):
         """Updates a property of a given list."""
         params = {
@@ -20,6 +31,15 @@ class Lists(object):
 
         }
         return self._client.request('/lists/edit', params)
+    
+    def replace(self, square_id, list_array):
+        """Updates a property of a given list."""
+        params = {
+            'square': square_id,
+            'items': list_array
+
+        }
+        return self._client.request('/lists/replace', params)
 
     def delete(self, square_id, key):
         """Updates a property of a given list."""
